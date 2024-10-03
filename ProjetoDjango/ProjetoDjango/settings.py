e8ec348c5880e122b74f96fa9c4fc6cc0c0e6cb3
isaamunhoz <isadora.munhoz.oliveira@gmail.com>
2024-10-03 01:20:53 -0300

    Comentarios adicionados

--- ProjetoDjango/ProjetoDjango/settings.py
+++ ProjetoDjango/ProjetoDjango/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'primeiroCrud.apps.PrimeirocrudConfig',
+    'primeiroCrud.apps.PrimeirocrudConfig', #app
 ]
 
 MIDDLEWARE = [
--- ProjetoDjango/primeiroCrud/models.py
+++ ProjetoDjango/primeiroCrud/models.py
@@ -3,10 +3,13 @@ from django.db import models
 # Create your models here.
 
 class Produto(models.Model):
-    nome = models.CharField(max_length=100)
-    descricao = models.TextField()
-    preco = models.DecimalField(max_digits=8, decimal_places=2)
+    nome = models.CharField(max_length=100) #nome do produto com o maximo de 100 caracteres
+
+    descricao = models.TextField() #descricao do produto com textos longos
+
+    preco = models.DecimalField(max_digits=8, decimal_places=2) #preco do produto de ate 8 digitos e duas casas decimais
 
     def __str__(self):
-       return self.nome
+       return self.nome #retorna o nome do do profuto quando o objeto é convertido para string
+        
         
