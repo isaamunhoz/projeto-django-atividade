3056735e23a693e33fe6d5bea496270f595e9b6c
isaamunhoz <isadora.munhoz.oliveira@gmail.com>
2024-10-03 00:10:04 -0300

    Model

--- ProjetoDjango/primeiroCrud/models.py
+++ ProjetoDjango/primeiroCrud/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Produto(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField()
+    preco = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+       return self.nome
+        
